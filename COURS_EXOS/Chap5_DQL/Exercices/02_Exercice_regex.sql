f4f9ce354f00acbeb7e9fdc85ac591af32d3667f
Antoine07 <antoine.lucsko@gmail.com>
2023-08-17 11:33:47 +0200

    exercices

---
+++ COURS_EXOS/Chap5_DQL/Exercices/02_Exercice_regex.sql
@@ -0,0 +1,15 @@
+/*
+Récupérez tous les noms des pilotes dont le code de leurs compagnies se terminent par R suivi de 2 caractères exactement.
+
+*/
+
+
+SELECT name, company
+FROM pilots 
+WHERE company REGEXP '.*r.{2}$';
+
+
+
+SELECT name, company
+FROM pilots 
+WHERE company REGEXP '[[:alnum:]]*R[[:alnum:]]{2}';
---
+++ COURS_EXOS/Chap5_DQL/Exercices/04_list_Exercice.sql
@@ -0,0 +1,129 @@
+/*
+01 Calculez la moyenne des heures de vol pour chaque compagnie.
+*/
+
+SELECT AVG(num_flying), company FROM pilots
+GROUP BY company;
+
+
+/*
+
+02 Calculez la moyenne des heures de vol des pilotes dont le bonus est de 500,
+par compagnie.
+*/
+
+SELECT company, AVG(num_flying) FROM pilots
+WHERE bonus = 500
+GROUP BY company;
+
+/*
+
+03 Sélectionnez les compagnies ayant plus d'un pilote, ainsi que leur nombre de pilotes.
+
+*/
+SELECT company, COUNT(certificate) as c
+FROM pilots 
+GROUP BY company
+HAVING c > 1;
+
+/*
+04 Nombre de pilotes par compagnie et par nombre d'heure de vol.
+*/
+SELECT company, num_flying, COUNT(certificate)
+FROM pilots 
+GROUP BY company, num_flying;
+
+-- Pareil mais par Bonus et pas par nb d'heure de vol
+SELECT company, bonus, COUNT(certificate)
+FROM pilots 
+GROUP BY company, bonus;
+
+/*
+05 Exercice ajout d'une colonne
+si ce n'est pas déjà fait (dans les exercices)
+*/
+
+ALTER TABLE pilots 
+ADD COLUMN plane 
+ENUM('A380', 'A320', 'A340') AFTER name;
+
+UPDATE pilots
+SET plane = 'A380'
+WHERE name in ('Alan', 'Sophie', 'Albert', 'Benoit');
+
+UPDATE pilots
+SET plane = 'A320'
+WHERE name in ('Tom', 'John', 'Yi');
+
+UPDATE pilots
+SET plane = 'A340'
+WHERE name in ('Yan', 'Pierre');
+
+/*
+05 Sélectionnez le nombre de pilotes par compagnie et par type d'avion.
+*/
+
+SELECT company, plane, COUNT(certificate)
+FROM pilots
+GROUP BY company, plane;
+
+
+/*
+
+06 
+
+- Sélectionnez le noms des pilotes par bonus.
+
+- Sélectionnez le nom et la compagnie des pilotes par bonus.
+*/
+
+-- separator par defaut : ","
+SELECT bonus, GROUP_CONCAT(name) as pilotes FROM pilots GROUP BY bonus;
+
+-- pas de virgule
+SELECT bonus, GROUP_CONCAT(name SEPARATOR ' ') as pilotes  FROM pilots GROUP BY bonus;
+
+-- Avec le code de la compagnie en plus :
+SELECT bonus, GROUP_CONCAT( `name`,' ', `company` ) as pilotes
+FROM pilots GROUP BY company, bonus;
+
+-- Plus propre :
+SELECT bonus, GROUP_CONCAT(`name`,' (', `company`,')' SEPARATOR ', ') as pilotes
+FROM pilots GROUP BY company, bonus;
+
+-- Attention vous avez également CONCAT qui permet de concaténer deux colonnes, ce n'est pas une fonction de 
+-- regroupement
+
+SELECT CONCAT(name, plane) from pilots;  -- sélectionne toutes les lignes
+
+/*
+
+07 Calculez l'étendue du nombre d'heures de vol par compagnie.
+
+*/
+
+SELECT company, MAX(num_flying) - MIN(num_flying)
+FROM pilots GROUP BY company;
+
+/*
+
+08 Faites la somme du nombre de jours de vols par compagnie 
+dont la somme est supérieure à 30.
+
+*/
+
+SELECT sum(num_jobs) as sum_jobs, company
+FROM pilots GROUP BY company
+HAVING sum_jobs > 30;
+
+-- Calculez le nombre de jours de vol par compagnie, supérieur à 30 jours
+
+SELECT ROUND( sum(num_flying) / 24, 1 ) as nbDay, company
+FROM pilots GROUP BY company
+HAVING nbDay > 30;
+
+
+/*
+09 Afficher la moyenne des heures de vol pour les compagnies qui sont en France.
+*/
+
