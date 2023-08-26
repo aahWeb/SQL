f8a3fa7f51f850df13a76a9ffba71ec77aa1e420
Antoine07 <antoine.lucsko@gmail.com>
2023-08-26 16:22:16 +0200

    sql

--- COURS_EXOS/Chap5_DQL/Exercices/05_avg_city.sql
+++ COURS_EXOS/Chap5_DQL/Exercices/05_avg_city.sql
@@ -6,3 +6,14 @@ Calculer la moyenne des heures de vols par pays.
 
 */
 
+SELECT c.city, AVG(p.num_flying) as avg_flying
+FROM pilots as p
+JOIN companies as c
+ON p.company = c.comp
+GROUP BY c.city ;
+
+SELECT c.country, AVG(p.num_flying) as avg_flying, GROUP_CONCAT(DISTINCT c.city) as city
+FROM pilots as p
+JOIN companies as c
+ON p.company = c.comp
+GROUP BY c.country ;
